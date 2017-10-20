ac29c09d9b32f1171a5e09acffb99460c2ea8a72
phpwizz <nedx.45@gmail.com>
2017-10-20 21:10:08 +0200

    Admin--Remove post v.07

--- app/src/main/java/world/emir/sparkairlines/NextFligthsFragment.java
+++ app/src/main/java/world/emir/sparkairlines/NextFligthsFragment.java
@@ -1,22 +1,30 @@
 package world.emir.sparkairlines;
 
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +32,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Map;
 
@@ -37,6 +46,7 @@ public class NextFligthsFragment extends Fragment {
     ImageButton popup_menu_btn;
     RecyclerView recyclerView;
     //DatabaseReference mDatabase;
+    ProgressDialog progressDialog;
 
 
     public NextFligthsFragment() {
@@ -63,7 +73,7 @@ public class NextFligthsFragment extends Fragment {
 
         //Firebase
 
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Flights");
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Flights");
 
         FirebaseRecyclerAdapter<Flights,FlightViewHolder> adapter = new FirebaseRecyclerAdapter<Flights, FlightViewHolder>(
                 Flights.class,
@@ -72,12 +82,141 @@ public class NextFligthsFragment extends Fragment {
                 mDatabase
         ) {
             @Override
-            protected void populateViewHolder(FlightViewHolder viewHolder, Flights model, int position) {
+            protected void populateViewHolder(final FlightViewHolder viewHolder, Flights model, int position) {
+
+                final String flight_key = getRef(position).getKey();
+
+
 
                 viewHolder.setTitle(model.getName_of_destination());
                 viewHolder.setDescription(model.getDescription());
                 viewHolder.setAuthor(model.getAuthor());
                 viewHolder.setThumbImage(getActivity(),model.getThumb_image());
+
+
+                FirebaseUser mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (mCurrentUser != null ) {
+
+                    String current_uid = mCurrentUser.getUid();
+
+                    DatabaseReference mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+                    mUserDatabase.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+
+                            String role = dataSnapshot.child("role").getValue().toString(); //Get role information
+
+                            if (role.equals("member")) {
+
+
+                            } else {
+
+                                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+
+
+                                        PopupMenu popup = new PopupMenu(getActivity(), viewHolder.mView);
+                                        //Inflating the Popup using xml file
+                                        popup.getMenuInflater().inflate(R.menu.pop_menu2, popup.getMenu());
+
+
+                                        //registering popup with OnMenuItemClickListener
+                                        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                                            public boolean onMenuItemClick(MenuItem item) {
+
+                                                int id = item.getItemId();
+                                                //noinspection SimplifiableIfStatement
+                                                if (id == R.id.add_biljesku) {
+
+                                                    Toast.makeText(getActivity(), "Add trip description", Toast.LENGTH_SHORT).show();
+                                                    Intent i = new Intent(getActivity(), AddNoteActiviy.class);
+                                                    getActivity().startActivity(i);
+
+                                                    return true;
+                                                } else if (id == R.id.see_biljesku) {
+
+                                                    Toast.makeText(getActivity(), "See trip description", Toast.LENGTH_SHORT).show();
+                                                    return true;
+                                                }else if (id == R.id.delete_flight) {
+
+                                                    //Create Database ref
+
+                                                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                                                    builder.setTitle("Remove this flight!");
+                                                    builder.setMessage("Are you sure?");
+
+                                                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                                                        public void onClick(final DialogInterface dialog, int which) {
+
+                                                            final ProgressDialog progressDialog = ProgressDialog.show(getActivity(), "Deleting...", "Please wait...", true);
+
+                                                            final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Flights/"+flight_key);
+                                                            mDatabase.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> task) {
+
+
+                                                                    progressDialog.dismiss();
+
+                                                                }
+                                                            });
+
+                                                        }
+                                                    });
+
+                                                    builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which) {
+
+                                                            // Do nothing
+                                                            dialog.dismiss();
+                                                        }
+                                                    });
+
+                                                    AlertDialog alert = builder.create();
+                                                    alert.show();
+
+
+
+
+                                                }
+
+
+                                                return true;
+                                            }
+                                        });
+
+                                        popup.setGravity(Gravity.RIGHT);
+
+                                        popup.show();//showing popup menu
+
+
+
+
+
+                                    }
+                                });
+
+
+
+                            }
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+
 
 
             }
